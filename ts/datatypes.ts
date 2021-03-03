24cc6b2829be544733b6f15022365240a4658e60
srikanthpragada <srikanthpragada@gmail.com>
2021-03-03 08:35:33 +0530

    03-MAR

---
+++ ts/datatypes.ts
@@ -0,0 +1,16 @@
+
+var username : string = "Srikanth";
+var email : string = null;
+var price = 100;
+var discounted = false;
+
+
+console.log(username)
+console.log(email);
+
+
+let uname : string = "Srikanth";
+let subject : string = "Angular";
+// String interpolation 
+let sentence: string =`Hello, I am ${uname } and I am your ${subject} trainer!`
+console.log(sentence)
---
+++ ts/hello.ts
@@ -0,0 +1,3 @@
+// Hello program
+
+console.log("Hello TypeScript 4.2");
